d42f54fd3ed2ebf583d3384d9669b3f1c48dbb29
Joacim Granberg <joacimgr@kth.se>
2020-02-06 14:18:57 +0100

    Add build desktop ver

---
+++ build/precache-manifest.960ada21936256a87b8b50be465bc457.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9728531b82c184bdfb6cc7ea7029d9fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa3ca9bce468f1c09f5",
+    "url": "/static/css/main.222ca434.chunk.css"
+  },
+  {
+    "revision": "47bf31833ca0f37e7c79",
+    "url": "/static/js/2.3c9e0c6f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.3c9e0c6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aa3ca9bce468f1c09f5",
+    "url": "/static/js/main.c26f7cfe.chunk.js"
+  },
+  {
+    "revision": "79e4784027847ad0f99f",
+    "url": "/static/js/runtime-main.ce6146d1.js"
+  },
+  {
+    "revision": "151fd71a1f27232e94e8bd368f5a6cd0",
+    "url": "/static/media/background.151fd71a.mp4"
+  },
+  {
+    "revision": "d41942e6d8d0339b3f8cf5c5f23c49fe",
+    "url": "/static/media/dela.d41942e6.png"
+  },
+  {
+    "revision": "3aa150c947ad45ac7bc4e078fd143ede",
+    "url": "/static/media/drom.3aa150c9.png"
+  },
+  {
+    "revision": "adc9f00ec1c551f1d6d36ad36f18d694",
+    "url": "/static/media/raster.adc9f00e.png"
+  },
+  {
+    "revision": "6c3451ef85b030e9d3023b051a1e2685",
+    "url": "/static/media/skapa.6c3451ef.png"
+  }
+]);
